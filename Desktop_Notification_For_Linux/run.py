8307e5eb756a3a959c3fabdcc0d9f8b11c281661
Apurva Nisal <apunisal@gmail.com>
2018-07-20 00:25:02 +0530

    Notify

---
+++ Desktop_Notification_For_Linux/run.py
@@ -0,0 +1,25 @@
+import time
+import notify2
+from topnews import topStories
+ 
+
+# fetch news items
+newsitems = topStories()
+ 
+# initialise the d-bus connection
+notify2.init("News Notifier")
+ 
+# create Notification object
+n = notify2.Notification(None, icon = ICON_PATH)
+ 
+
+n.set_urgency(notify2.URGENCY_NORMAL)
+ 
+
+n.set_timeout(10000)
+ 
+for newsitem in newsitems:
+ 
+    n.update(newsitem['title'], newsitem['description'])
+    n.show()
+    time.sleep(15)
